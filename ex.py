a2fa068f9afd6d85c5b697b49aaa67b06a43b006
sunhl4 <15051962326@163.com>
2023-08-24 22:04:20 +0800

    new

--- ex.py
+++ ex.py
@@ -1,29 +1,40 @@
 import jax.numpy as jnp
-from jax import random
+import jax
+from jax import random, grad
+from sklearn.datasets import load_iris
+data = load_iris()
+iris_data = jnp.float32(data.data)
+iris_target = jnp.float32(data.target)
+def Dense(dense_shape = [4, 1]):
+    def init_fun(input_shape = dense_shape):
+        rng = random.PRNGKey(17)
+        w, b = random.normal(rng, shape=input_shape), random.normal(rng, shape=(input_shape[-1],))
+        return w, b
+    def apply_function(inputs, params):
+        w, b = params
+        return jnp.dot(inputs, w) + b
+    return init_fun, apply_function
+init_fun, apply_function = Dense()
+params = init_fun()
+
+def loss_linear(params, x, y):
+    preds = apply_function(x, params)
+    return jnp.mean(jnp.power(preds - y,2.0))
+
+learning_rate = 0.005
+
+N = 1000
+
+for i in range(N):
+    loss = loss_linear(params, iris_data, iris_target)
+    if i % 100 ==0:
+        print(f'i: {i}, loss: {loss}')
+    params_grad = jax.grad(loss_linear)
+    params = [(p-g*learning_rate) for p, g in zip(params, params_grad(params, iris_data, iris_target))]
+print(f'i: {N}, loss: {loss}')
 
 
-weight = jnp.array([[1.7, 1.7],[2.14,2.14]])
-mat_a = jnp.array([[1],[2]])
-bias = 0.99
-#print (jnp.matmul(weight, mat_a) + bias)
 
-def Dense (dense_shape = [2, 1]):
-    rng = random.PRNGKey(17)
-    mat_a = random.normal (rng, shape = dense_shape)
-    bias = random.normal(rng, shape = (dense_shape[-1],) )
-    params = [mat_a, bias]
-
-    def init_parm():
-        return params
-    def apply_function(inputs, params = params):
-        mat_a, b = params
-        return jnp.dot(inputs,mat_a) +b
-    return apply_function
-weight = jnp.array([[1.7, 1.7],[2.14,2.14]])
-res = Dense()(weight)
-#print (res)
-
-from sklearn.datasets  import load_iris
 
 
 
