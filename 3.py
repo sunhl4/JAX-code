84578d6892514ee4abd2b3f225e5a0e00f47ad9e
sunhl4 <15051962326@163.com>
2023-08-27 11:55:26 +0800

    new

--- 3.py
+++ 3.py
@@ -2,16 +2,45 @@ import time
 import jax
 import jax.numpy as jnp
 from jax import lax
+x =jnp.arange(5)
+w = jnp.array([2., 3., 4.])
+def convolve(x, w):
+    output = []
+    for i in range(1, len(x)-1):
+        output.append(jnp.dot(x[i-1:i+2], w))
+    return jnp.array(output)
+# print(convolve(x, w))
+xs = jnp.stack([x, x])
+ws = jnp.stack([w, w])
+# print(ws)
+
+def manually_batched_convolve(xs, ws):
+    output = []
+    for i in range(xs.shape[0]):
+        output.append(convolve(xs[i], ws[i]))
+    return jnp.stack(output)
+# print(manually_batched_convolve(xs, ws))
+
+def manually_vectorized_convolve(xs, ws):
+    output = []
+    for i in range(1, xs.shape[-1] -1):
+        output.append(jnp.sum(xs[:,i-1:i+2] * ws, axis=1))
+    return jnp.stack(output, axis=1)
+print(manually_vectorized_convolve(xs, ws))
+
+
+
 
 # @jax.jit
-def loop_body(prev_i):
-    return prev_i +1
-def g_inner_jitted(x, n):
-    i = 0
-    while i < n:
-        i = jax.jit(loop_body)(i)
-    return x + i
-print(g_inner_jitted(10, 20))
+
+# def loop_body(prev_i):
+#     return prev_i +1
+# def g_inner_jitted(x, n):
+#     i = 0
+#     while i < n:
+#         i = jax.jit(loop_body)(i)
+#     return x + i
+# print(g_inner_jitted(10, 20))
 
 # def body_fun(x,y):
 #     return x*y, x**2+y**2
